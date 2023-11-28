5137c332f4041f16ebbb889dc1f1b73292b878b5
JIYEON-H <jiyeon.heo0906@gmail.com>
2023-11-28 13:34:54 -0700

    Organzed files

--- app/components/form.js
+++
@@ -1,7 +0,0 @@
-import React, { useState } from "react";
-import { GoogleMap } from "./googleMap";
-
-export function Form() {
-  const [address, setAddress] = useState("");
-  const [submittedAddress, setSubmittedAddress] = useState("");
-}
