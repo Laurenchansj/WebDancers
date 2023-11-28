24db4c4cdaef5ccb661058f70e9ad58dd4567427
JIYEON-H <jiyeon.heo0906@gmail.com>
2023-11-28 12:29:27 -0700

    modified page.js

--- app/page.js
+++ app/page.js
@@ -5,13 +5,6 @@ import React from "react";
 import BlogHome from "../pages/blogHome";
 
 export default function Home() {
-  // function initMap() {
-  //   const map = new google.maps.Map(document.getElementById("map"), {
-  //     center: { lat: -34.397, lng: 150.644 },
-  //     zoom: 8,
-  //   });
-  // }
-
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <Nav />
