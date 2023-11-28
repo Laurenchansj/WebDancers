b7a77fd3ed6abe73c09f1cc49b638191a60bb81e
JIYEON-H <jiyeon.heo0906@gmail.com>
2023-11-28 13:21:35 -0700

    rebase main

--- app/page.js
+++ app/page.js
@@ -6,13 +6,11 @@ import BlogHome from "../pages/blogHome";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <Nav />
       <div className='z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex'>
         <BlogHome />
       </div>
-      
-
     </main>
-  )
+  );
 }
